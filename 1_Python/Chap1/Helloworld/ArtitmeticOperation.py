7d405ee8e7cc7386fcadc258229b0ea94fe8331b
AvinashTiwari85 <qwe123kids@gmail.com>
2019-10-29 23:34:47 -0600

    no message

---
+++ 1_Python/Chap1/Helloworld/ArtitmeticOperation.py
@@ -0,0 +1,19 @@
+age1 =12
+age2 = 18
+print(age1 + age2)
+print(age1 - age2)
+print(age1 * age2)
+print(age1 / age2)
+print(age1 % age2)
+
+sent1 =  "today was beautiful day"
+Firstname = "Avinash"
+LastName = "Tiwari"
+
+print(Firstname + " " + LastName)
+
+print("Hi" * 10)
+
+sentence = "Avinash was Playing "
+print(sentence[0])
+print(sentence[0:7])
---
+++ 1_Python/Chap1/Helloworld/PlaceHolder.py
@@ -0,0 +1,15 @@
+name = "Jake"
+name = name + " is 15 years old"
+print(name)
+
+sent = " %s is 15 year old"
+
+print(sent)
+name = "Avinash"
+print(sent%name)
+
+sent = "%s %s is president of USA"
+print(sent%("Donald", "trump"))
+
+sent = "%s is %d is year old"
+print(sent%("Donald", 15))
