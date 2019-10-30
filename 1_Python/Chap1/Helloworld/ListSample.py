92e0f8ccf9be9633c8122e6cfe25933dd1f18ab2
AvinashTiwari85 <qwe123kids@gmail.com>
2019-10-30 12:54:45 -0600

    no message

---
+++ 1_Python/Chap1/Helloworld/ListSample.py
@@ -0,0 +1,14 @@
+shopList = ["Apples","Oranges","Banana","Cheese"]
+print(shopList)
+print(shopList[0])
+print(shopList[0:2])
+shopList.append("blueberry")
+print(shopList)
+shopList[0] = "Cherry"
+print(shopList)
+print(len(shopList))
+shopList2 = ["bread", "jam", "peanutbutter"]
+print(shopList + shopList2)
+listNum = [1,2,3,4,5,6,7]
+print(max(listNum))
+print(min(listNum))
