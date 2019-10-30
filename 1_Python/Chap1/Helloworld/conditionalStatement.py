1cc50046b3dd58c54660501df11dc46929f95b96
AvinashTiwari85 <qwe123kids@gmail.com>
2019-10-30 17:35:44 -0600

    no message

---
+++ 1_Python/Chap1/Helloworld/conditionalStatement.py
@@ -0,0 +1,19 @@
+if (5 > 3):
+    print("Hello")
+else:
+    print("not Hello")
+
+if(3 < 2):
+    print("hello")
+else:
+    print("Contion failed")
+
+
+age = 16
+
+if(age < 13):
+    print("you are youg")
+elif(age >= 13 and age <18):
+    print("you are teen ager")
+else:
+    print("you are adult")
---
+++ 1_Python/Chap1/Helloworld/forloop.py
@@ -0,0 +1,12 @@
+list1 = ["Apples", "banana","Cherries"]
+tup = (1,2,3)
+
+for item in list1:
+    print(item)
+
+for item in tup:
+    print(item)
+
+for i in range(0,10):
+    print(i)
+
