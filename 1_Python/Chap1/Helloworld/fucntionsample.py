5590254c570807d250d0324cd76156ff2ec67fbf
AvinashTiwari85 <qwe123kids@gmail.com>
2019-11-02 15:15:00 -0600

    no message

---
+++ 1_Python/Chap1/Helloworld/fucntionsample.py
@@ -0,0 +1,16 @@
+def Helloworld():
+    print("Hello World")
+
+Helloworld()
+
+def Greeting(name):
+    print("Hi " + name)
+
+Greeting("Avinash")
+
+def returnAdd(num1, num2):
+    return (num1 + num2)
+
+sum = returnAdd(1,2)
+print(sum)
+
