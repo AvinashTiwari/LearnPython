70b238f55b62ea75376128f9b1d7e1193d6c0b3b
AvinashTiwari85 <qwe123kids@gmail.com>
2019-11-12 17:43:09 -0700

    no message

--- 1_Python/Chap1/Helloworld/webscrapping/WebScarpping.py
+++ 1_Python/Chap1/Helloworld/webscrapping/WebScarpping.py
@@ -5,3 +5,11 @@ data = requests.get("https://darksky.net/forecast/39.5223,-104.867/us12/en")
 
 soup = BeautifulSoup(data.text,'html.parser')
 print(soup.prettify())
+#print(soup.head())
+#print(soup.body())
+#print(soup.title)
+paragraph = soup.body.div
+print(type(paragraph))
+print(paragraph.text)
+print(paragraph['class'])
+print(paragraph.attrs)
