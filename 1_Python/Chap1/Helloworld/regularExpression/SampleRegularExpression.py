ea169ec3edd7d16a55a6e90e2dfa9fb025478a0b
AvinashTiwari85 <qwe123kids@gmail.com>
2019-11-16 16:43:46 -0700

    no message

--- 1_Python/Chap1/Helloworld/regularExpression/SampleRegularExpression.py
+++ 1_Python/Chap1/Helloworld/regularExpression/SampleRegularExpression.py
@@ -13,3 +13,56 @@ Sentence = "I love fruits , I all love sort of berries , i am fan of strawberrie
 fruitRegex = re.compile(r'strawberries|blueberries')
 fruit = fruitRegex.search(Sentence)
 print(fruit.group())
+
+phoneNumerRegex = re.compile(r'(\d\d\d-)?\d\d\d-\d\d\d\d')
+mo = phoneNumerRegex.search("this my number 123-456-7890")
+print(mo)
+mo = phoneNumerRegex.search("this my number 456-7890")
+print(mo)
+
+batRegex = re.compile(r'Bat(wo)*man')
+bat = batRegex.search("I am Batwoman")
+print(bat)
+bat = batRegex.search("I am Batwowowowoman")
+print(bat)
+bat = batRegex.search("I am Batman")
+print(bat)
+
+batRegex = re.compile(r'Bat(wo)+man')
+bat = batRegex.search("I am Batwoman")
+
+phoneNumerRegex = re.compile(r'\D\D\D')
+mo = phoneNumerRegex.search('123 asd')
+print(mo)
+
+phoneNumerRegex = re.compile(r'\w\w\w')
+mo = phoneNumerRegex.search('asd')
+print(mo)
+
+phoneNumerRegex = re.compile(r'\W\W\W')
+mo = phoneNumerRegex.search('asd***')
+print(mo)
+
+phoneNumerRegex = re.compile(r'\s\w+')
+mo = phoneNumerRegex.search(' asd***')
+print(mo)
+
+phoneNumerRegex = re.compile(r'\d+\s\w+')
+mo = phoneNumerRegex.findall('12 peechs 14 lemon 15 cherries')
+print(mo)
+
+vowl = re.compile(r'[AEIOUaeiou]')
+vo = vowl.findall('This is a sentence vowl')
+print(vo)
+
+vowl = re.compile(r'[^AEIOUaeiou]')
+vo = vowl.findall('This is a sentence vowl')
+print(vo)
+
+beginning = re.compile(r'^Hello')
+vo = beginning.findall('Hello Avinash')
+print(vo)
+
+beginning = re.compile(r'\d+$')
+vo = beginning.findall('Avinash 1221')
+print(vo)
