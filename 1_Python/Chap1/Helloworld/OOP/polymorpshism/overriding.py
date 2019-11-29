5a877c4f2b0c8e2f207d37f17224ea6e9d5594db
AvinashTiwari85 <qwe123kids@gmail.com>
2019-11-28 17:32:51 -0700

    no message

---
+++ 1_Python/Chap1/Helloworld/OOP/polymorpshism/overriding.py
@@ -0,0 +1,10 @@
+class Employee:
+    def numberOfWorkingHour(self):
+        self.numberOfowrkinghour = 40
+
+    def displayNumberOfWorkingHours(self):
+        print(self.numberOfowrkinghour)
+
+employee =  Employee()
+employee.numberOfWorkingHour()
+print(employee.displayNumberOfWorkingHours())
