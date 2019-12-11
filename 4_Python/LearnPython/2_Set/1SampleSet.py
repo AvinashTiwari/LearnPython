16472e87bd2aa32d73dc0c377445c9655e344948
AvinashTiwari85 <qwe123kids@gmail.com>
2019-12-10 19:32:02 -0700

    no message

---
+++ 4_Python/LearnPython/2_Set/1SampleSet.py
@@ -0,0 +1,14 @@
+list = [1,2,3,4,5,6,6]
+
+print(set(list))
+
+set1 = set([11,12,14,14,11,12])
+print(set1)
+print(type(set1))
+
+set2 = {11,12,12,14,14}
+print(set2)
+print(type(set2))
+print(len(set2))
+print(11 in set2)
+print(20 in set2)
