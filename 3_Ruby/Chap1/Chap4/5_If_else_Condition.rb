a25a1a20e5c06f26e4f539f15f7fb0a282554247
AvinashTiwari85 <qwe123kids@gmail.com>
2019-12-10 18:10:49 -0700

    no message

---
+++ 3_Ruby/Chap1/Chap4/5_If_else_Condition.rb
@@ -0,0 +1,8 @@
+if 5 < 2
+  puts "True"
+end
+
+password ="Test1"
+if password == "Test"
+  puts "Inside"
+end
---
+++ 4_Python/LearnPython/1_ListSample/ListSlice.py
@@ -0,0 +1,11 @@
+list3 = [1,2,3,"a","b","c"]
+
+print(list3[0:3])
+print(list3[:3])
+print(list3[2:3])
+print(list3[3:])
+print(list3[-1])
+print(list3[-2])
+print(list3[-4:-1])
+print(list3[-3:])
+print(list3[::2])
