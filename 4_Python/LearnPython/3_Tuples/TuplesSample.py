82470f7f3ae46c36b92ba947e46ef79102396c26
AvinashTiwari85 <qwe123kids@gmail.com>
2019-12-12 19:35:43 -0700

    no message

---
+++ 4_Python/LearnPython/3_Tuples/TuplesSample.py
@@ -0,0 +1,21 @@
+my_tuple = ()
+print(type(my_tuple))
+
+my_tuple = (9,)
+
+my_tuple = (1,2,3,4,5)
+print(my_tuple)
+
+print(my_tuple[1])
+
+tuple1 =  ("Cisco", "2600", "12.4")
+
+A,B,C = tuple1
+print(A)
+
+tuple2 = (1,2,3,4)
+print(len(tuple2))
+print(max(tuple2))
+print(min(tuple2))
+
+
