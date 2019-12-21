2880380a60b92cee767c01f7cf64937b436ecb34
AvinashTiwari85 <qwe123kids@gmail.com>
2019-12-20 23:37:59 -0700

    no message

---
+++ 3_Ruby/Chap1/Chap6/2_AlphabeticRangne.rb
@@ -0,0 +1,6 @@
+puts (1..10).first(4)
+
+alpabets = "a"..."z"
+puts alpabets.size
+puts alpabets.first(5)
+puts alpabets.last(5)
---
+++ 4_Python/LearnPython/8_function/functionSample.py
@@ -0,0 +1,5 @@
+def my_first_function():
+    "This first function"
+    print("hello")
+
+help(my_first_function())
