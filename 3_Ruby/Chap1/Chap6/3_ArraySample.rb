480c31a905dd6885c9e244eafd6eac3b1f8ae202
AvinashTiwari85 <qwe123kids@gmail.com>
2019-12-22 12:11:53 -0700

    no message

---
+++ 3_Ruby/Chap1/Chap6/3_ArraySample.rb
@@ -0,0 +1,10 @@
+p []
+p [].class
+
+numbers = [1,2,3,4,5,6]
+p numbers
+puts numbers
+
+data = [["1",2,3],
+        ["3",4,5]]
+puts data
--- 4_Python/LearnPython/8_function/functionSample.py
+++ 4_Python/LearnPython/8_function/functionSample.py
@@ -3,3 +3,17 @@ def my_first_function():
     print("hello")
 
 help(my_first_function())
+
+
+def my_first_function_arg(x,y,z):
+    sum_xyz = (x+y)*z
+    return sum_xyz
+
+print(my_first_function_arg(x=1,y=2,z=3))
+
+def function1(x, *args):
+    print(x)
+    for argument in args:
+        print(argument)
+
+print(function1(1,2,3))
