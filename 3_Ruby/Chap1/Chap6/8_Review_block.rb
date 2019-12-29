38e1e7434f179f483cd9c162e796ee4cfc019756
AvinashTiwari85 <qwe123kids@gmail.com>
2019-12-28 19:08:07 -0700

    no message

---
+++ 3_Ruby/Chap1/Chap6/8_Review_block.rb
@@ -0,0 +1,10 @@
+5.times do |number|
+  sqaure = number * number
+  puts "The current number is #{number} and its square is #{sqaure}"
+end
+
+candies = ["Sour", "milky", "Airheads"]
+
+candies.each do |candy|
+  puts "I lov to eat candy #{candy}"
+end
